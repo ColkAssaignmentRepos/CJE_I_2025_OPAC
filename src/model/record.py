e30e4179b0c1f3da3dd73ce4d95f0e61aa0d973a
Colk-tech <master@colk.dev>
2025-07-16 03:14:04 +0900

    add: add `DcndlSimple` model

---
+++ src/model/record.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Optional, List
+
+from pydantic import BaseModel, Field
+
+
+class TypedValue(BaseModel):
+    value: str
+    type: Optional[str] = None
+
+
+class ResourceLink(BaseModel):
+    resource: str
+
+
+class Header(BaseModel):
+    identifier: str
+    datestamp: datetime
+
+
+class DcndlSimple(BaseModel):
+    title: str
+    identifier: List[TypedValue] = Field(..., min_length=1)
+    creator: List[str] = []
+    publisher: Optional[str] = None
+    alternative: Optional[str] = None
+    series_title: Optional[str] = None
+    date: Optional[str] = None
+    language: Optional[str] = None
+    extent: Optional[str] = None
+    material_type: Optional[str] = None
+    access_rights: Optional[str] = None
+    title_transcription: Optional[str] = None
+    volume: Optional[str] = None
+    publication_place: List[TypedValue] = []
+    issued: List[TypedValue] = []
+    subject: List[TypedValue] = []
+    see_also: List[ResourceLink] = []
+    same_as: List[ResourceLink] = []
+    thumbnail: List[ResourceLink] = []
+
+
+class Metadata(BaseModel):
+    dc: DcndlSimple
+
+
+class Record(BaseModel):
+    header: Header
+    metadata: Metadata
