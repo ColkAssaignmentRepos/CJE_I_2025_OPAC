b7e1c4a294346e1b3584a782bff31397a94ab8a8
Colk-tech <master@colk.dev>
2025-07-16 04:12:55 +0900

    add: add `ASYNC_DATABASE_URL` in `config.py`

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ class Config(BaseSettings):
     DATABASE_FILE_PATH: Path = Path(
         "/mount/gdrive/My Drive/cje1s2513929/database.sqlite3"
     )
+
+    @property
+    def ASYNC_DATABASE_URL(self) -> str:
+        # Use an in-memory SQLite database for default/testing if the path is not set
+        if not self.DATABASE_FILE_PATH:
+            return "sqlite+aiosqlite://"
+
+        return f"sqlite+aiosqlite:///{self.DATABASE_FILE_PATH.resolve()}"
