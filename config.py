5508097e42103d62f7fc29f78e3e4d3a872b25ff
Colk-tech <master@colk.dev>
2025-07-16 03:15:39 +0900

    fix: fix due to ruff

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    DATABASE_FILE_PATH: Path = Path("/mount/gdrive/My Drive/cje1s2513929/database.sqlite3")
+    DATABASE_FILE_PATH: Path = Path(
+        "/mount/gdrive/My Drive/cje1s2513929/database.sqlite3"
+    )
